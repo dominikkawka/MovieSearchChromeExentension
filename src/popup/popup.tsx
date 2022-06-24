0f94a0d1c72a9704f2989bad6ac0a84391ca84f5
dominikkawka <dominikkawka2002@gmail.com>
2022-06-24 13:03:10 +0100

    added search bar and search feature

--- src/popup/popup.tsx
+++ src/popup/popup.tsx
@@ -1,13 +1,18 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import ReactDOM from 'react-dom'
 import './popup.css'
 import { fetchMovieSearchData, MovieSearchData } from '../utils/api'
 import MovieCard from '../movieCard/movieCard'
+import { TextInput } from '@patternfly/react-core'
 
 const App: React.FC<{}> = () => {
 
+  const [movieInput, setMovieInput] = useState<string>('')
   return (
-    <div><MovieCard title="Marvel"></MovieCard></div>
+    <div>
+      <TextInput id="searchBar" iconVariant="search" value={movieInput} onChange={(event) => setMovieInput(event)}/>
+      <MovieCard title={movieInput}></MovieCard>
+    </div>
   )
 }
 
